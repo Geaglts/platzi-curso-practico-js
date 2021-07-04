e46d129f11935bb9f95458bb0dd7415a82570d46
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-07-03 20:56:17 -0500

    Formulario de calculos geometricos con HTML

--- figuras.js
+++ figuras.js
@@ -1,65 +1,72 @@
-function cl(msg) {
-  console.log(msg);
-}
-// Código del cuadrado
-console.group('Cuadrados');
-const ladoCuadrado = 5;
-cl('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm');
-
 function perimetroCuadrado(lado) {
   return lado * 4;
 }
-cl('El perímetro del cuadrado es: ' + perimetroCuadrado(ladoCuadrado) + 'cm');
 
 function areaCuadrado(lado) {
   return lado * lado;
 }
-cl('El área del cuadrado es: ' + areaCuadrado(ladoCuadrado) + 'cm^2');
-console.groupEnd();
-// Código del triángulo
-console.group('Triangulos');
-const ladoTriangulo1 = 3;
-const ladoTriangulo2 = 5;
-const baseTriangulo = 3;
-const alturaTriangulo = 4;
-cl(
-  `Los lados del triángulo son: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`
-);
 
 function perimetroTriandulo(lado1, lado2, base) {
-  return lado1 + lado2 + base;
+  return Number(lado1) + Number(lado2) + Number(base);
 }
-cl(
-  'El perímetro del triángulo es: ' +
-    perimetroTriandulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo) +
-    'cm'
-);
 
 function areaTriangulo(base, altura) {
   return (base * altura) / 2;
 }
-cl(
-  'El área del triángulo es: ' +
-    areaTriangulo(baseTriangulo, alturaTriangulo) +
-    'cm'
-);
-console.groupEnd();
-// Código del círculo
-console.group('Círculos');
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo * 2;
-cl('El radio del círculo es: ' + radioCirculo + 'cm');
-cl('El diametro del círculo es: ' + diametroCirculo + 'cm');
 
 const diamretoCirculo = (radio) => radio * 2;
 
 function areaCirculo(radio) {
   return Math.PI * (radio * radio);
 }
-cl(`El área del circulo es: ${areaCirculo(radioCirculo)}cm`);
 
 function perimetroCirculo(radio) {
   return Math.PI * diamretoCirculo(radio);
 }
-cl(`El perímetro del circulo es: ${perimetroCirculo(radioCirculo)}cm`);
-console.groupEnd();
+
+// Manipulación del HTML
+const resultadoCuadrado = document.getElementById('resultado-cuadrado');
+const resultadoTriangulo = document.getElementById('resultado-triangulo');
+const resultadoCirculo = document.getElementById('resultado-circulo');
+// Cuadrados
+function calcularAreaCuadrado() {
+  const { value } = document.getElementById('lado-cuadrado');
+  const area = areaCuadrado(value);
+  resultadoCuadrado.innerText = `El área del cuadrado es ${area}cm^2`;
+}
+
+function calcularPerimetroCuadrado() {
+  const { value } = document.getElementById('lado-cuadrado');
+  const area = perimetroCuadrado(value);
+  resultadoCuadrado.innerText = `El perímetro del cuadrado es ${area}cm`;
+}
+
+// Tríangulos
+function calcularAreaTriangulo() {
+  const { value: base } = document.getElementById('base-triangulo');
+  const { value: altura } = document.getElementById('altura-triangulo');
+  const area = areaTriangulo(base, altura);
+  resultadoTriangulo.innerText = `El área del triangulo es ${area}cm`;
+}
+
+function calcularPerimetroTriangulo() {
+  const { value: lado1 } = document.getElementById('lado1-triangulo');
+  const { value: lado2 } = document.getElementById('lado2-triangulo');
+  const { value: base } = document.getElementById('base-triangulo');
+
+  const perimetro = perimetroTriandulo(lado1, lado2, base);
+  resultadoTriangulo.innerText = `El perímetro del triangulo es ${perimetro}cm`;
+}
+
+// Círculo
+function calcularAreaCirculo() {
+  const { value: radio } = document.getElementById('radio-circulo');
+  const area = areaCirculo(radio);
+  resultadoCirculo.innerText = `El área del circulo es ${area}cm`;
+}
+
+function calcularPerimetroCirculo() {
+  const { value: radio } = document.getElementById('radio-circulo');
+  const perimetro = perimetroCirculo(radio);
+  resultadoCirculo.innerText = `El perímetro del circulo es ${perimetro}cm`;
+}
