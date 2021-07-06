ebe13c407db8064a3eb5aa88d37a95f4ecbde60b
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-07-06 17:55:16 -0500

    Reto terminado: Funciones mas inteligentes

--- precios-descuentos/descuentos.js
+++ precios-descuentos/descuentos.js
@@ -14,3 +14,29 @@ calcularDescuentoBtn.onclick = () => {
   const precioConDescuento = generarPrecioConDescuento(precio, descuento);
   resultado.innerText = `El precio con descuento es de $${precioConDescuento}`;
 };
+
+function calcularDescuentoConCupon(precio, cupon = '') {
+  const cupones = { TOP: 15, PLATZI: 50, CUPON: 75 };
+  if (cupones[cupon]) {
+    return generarPrecioConDescuento(precio, cupones[cupon]);
+  } else {
+    return 0;
+  }
+}
+
+const descuentoConCuponBtn = document.getElementById('descuento-cupon-btn');
+descuentoConCuponBtn.onclick = () => {
+  const htmlForm = document.getElementById('form-descuento-cupon');
+  const form = new FormData(htmlForm);
+  const precio = Number(form.get('precio'));
+  const cupon = form.get('cupon').toUpperCase();
+
+  // Resultado en HTML
+  const resultado = document.getElementById('descuento-cupon-resultado');
+  const precioConCupon = calcularDescuentoConCupon(precio, cupon);
+  if (precioConCupon === 0) {
+    resultado.innerText = `Este cupon no es valido`;
+  } else {
+    resultado.innerText = `El precio con el cupon es de $${precioConCupon}`;
+  }
+};
