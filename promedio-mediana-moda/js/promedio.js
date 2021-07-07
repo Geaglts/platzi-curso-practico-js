39452cfdac35fb8d4ad8960a913f687295beec98
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-07-06 21:25:07 -0500

    Calculo de la moda

--- promedio-mediana-moda/js/promedio.js
+++ promedio-mediana-moda/js/promedio.js
@@ -6,6 +6,12 @@ function generarValores(htmlId) {
   return valoresNumericos;
 }
 
+function modasToString(arreglo) {
+  let cadena = '';
+  arreglo.forEach((e) => (cadena += `${e[0]},`));
+  return cadena.replace(/\,$/, '.');
+}
+
 function calcularPromedio(valores) {
   if (valores.length > 0) {
     const sumaDeValores = valores.reduce(
@@ -36,6 +42,23 @@ function calcularMediana(valores) {
   }
 }
 
+function calcularModa(valores) {
+  const repeticiones = {};
+  valores.forEach((valor) => {
+    repeticiones[valor]
+      ? (repeticiones[valor] += 1)
+      : (repeticiones[valor] = 1);
+  });
+  const repeticionesOrdenadas = Object.entries(repeticiones).sort(
+    (a, b) => -(a[1] - b[1])
+  );
+  const valorMayor = repeticionesOrdenadas[0];
+  const posiblesModas = repeticionesOrdenadas.filter(
+    (r) => r[1] === valorMayor[1]
+  );
+  return posiblesModas;
+}
+
 // Promedio
 const calcularPromedioBtn = document.getElementById('calcular-promedio');
 calcularPromedioBtn.onclick = () => {
@@ -61,5 +84,22 @@ calcularMedianaBtn.onclick = () => {
     resultadoP.innerText = `Por favor coloca los valores con el formato correcto 🥀`;
   } else {
     resultadoP.innerText = `El mediana de los valores es ${mediana}`;
+  }
+};
+
+// Moda
+const calcularmodaBtn = document.getElementById('calcular-moda');
+calcularmodaBtn.onclick = () => {
+  const valores = generarValores('form-moda');
+  // Resultado en HTML
+  const resultadoP = document.getElementById('resultado-moda');
+  const posiblesModas = calcularModa(valores);
+  if (posiblesModas.length === 0 || posiblesModas[0][0] === 'NaN') {
+    resultadoP.innerText = `Por favor coloca los valores con el formato correcto 🥀`;
+  } else if (posiblesModas.length === 1) {
+    resultadoP.innerText = `La moda de los valores es ${posiblesModas[0][0]}`;
+  } else {
+    const modas = modasToString(posiblesModas);
+    resultadoP.innerText = `Las modas de los valores son ${modas}`;
   }
 };
