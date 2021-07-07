375244428010e06f686b0d61147bb55571b4481e
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-07-06 19:40:25 -0500

    Calculo de la mediana de pares e impares

--- promedio-mediana-moda/js/promedio.js
+++ promedio-mediana-moda/js/promedio.js
@@ -1,3 +1,11 @@
+function generarValores(htmlId) {
+  const htmlForm = document.getElementById(htmlId);
+  const form = new FormData(htmlForm);
+  const valores = form.get('valores').trim().split(',');
+  const valoresNumericos = valores.map((valor) => Number(valor));
+  return valoresNumericos;
+}
+
 function calcularPromedio(valores) {
   if (valores.length > 0) {
     const sumaDeValores = valores.reduce(
@@ -9,13 +17,28 @@ function calcularPromedio(valores) {
   return 0;
 }
 
-function generarValores(htmlId) {
-  const htmlForm = document.getElementById(htmlId);
-  const form = new FormData(htmlForm);
-  const valores = form.get('valores').trim().split(',');
-  return valores;
+function verificarParidad(valores, paridad) {
+  for (let valor of valores) {
+    if (valor % paridad !== 0) return false;
+  }
+  return true;
 }
 
+function calcularMediana(valores) {
+  const listaPar = verificarParidad(valores, 2);
+  if (listaPar) {
+    const primerValor = valores[Math.floor(valores.length / 2) - 1];
+    const segundoValor = valores[Math.floor(valores.length / 2)];
+    return (primerValor + segundoValor) / 2;
+  }
+  const listaImpar = verificarParidad(valores, 3);
+  if (listaImpar) {
+    return valores[Math.floor(valores.length / 2)];
+  }
+  return 0;
+}
+
+// Promedio
 const calcularPromedioBtn = document.getElementById('calcular-promedio');
 calcularPromedioBtn.onclick = () => {
   const valores = generarValores('form-promedio');
@@ -26,5 +49,19 @@ calcularPromedioBtn.onclick = () => {
     resultadoP.innerText = `Por favor coloca los valores con el formato correcto 🥀`;
   } else {
     resultadoP.innerText = `El promedio de los valores es ${promedio}`;
+  }
+};
+
+// Mediana
+const calcularMedianaBtn = document.getElementById('calcular-mediana');
+calcularMedianaBtn.onclick = () => {
+  const valores = generarValores('form-mediana');
+  // Resultado en HTML
+  const resultadoP = document.getElementById('resultado-mediana');
+  const mediana = calcularMediana(valores);
+  if (isNaN(mediana)) {
+    resultadoP.innerText = `Por favor coloca los valores con el formato correcto 🥀`;
+  } else {
+    resultadoP.innerText = `El mediana de los valores es ${mediana}`;
   }
 };
